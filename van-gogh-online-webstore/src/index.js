e48bc80d1ed79faea7a9a49c0f94f9f25ace1a5d
Yasmin Araujo <52540870+yasmin-araujo@users.noreply.github.com>
2023-05-17 13:29:44 -0300

    Add Router and organize files
    - Remove unnecessary files
    - Create HomePage files
    - Add routing dependencies

--- van-gogh-online-webstore/src/index.js
+++ van-gogh-online-webstore/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './style.css';
 import reportWebVitals from './reportWebVitals';
+
+import HomePage from './pages/HomePage';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" component={HomePage} />
+			</Switch>
+		</BrowserRouter>
+	</StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
