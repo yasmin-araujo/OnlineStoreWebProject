c13807a173d87b3497f5df74c74954ce5e57f3a9
tiagocbr <111984576+tiagocbr@users.noreply.github.com>
2023-05-22 04:43:04 -0300

    Add files via upload

---
+++ van-gogh-online-webstore/src/pages/SignIn/index.jsx
@@ -0,0 +1,46 @@
+import React,{ useState } from 'react'
+import './style.css'
+import Navbar from '../../components/Navbar'
+import Input from '../../components/TextInput'
+import Button from '../../components/Button'
+
+
+const SignIn = () => {
+
+    const [inputData1,setInputData1]= useState('')
+    const [inputData2,setInputData2]= useState('')
+
+    const onClick = () => {
+
+        console.log(inputData1);
+        console.log(inputData2);
+
+    }
+
+    return <>
+    <Navbar /><br/><br/><br/>
+
+<div>
+    <div className='meio-signin'>
+       <div className='signin'>
+       <span >Sign In</span> 
+       </div>
+       <div>  
+
+        <Input setInputData={setInputData1} placeholder={"Email"} type={"email"}/>
+        <Input setInputData={setInputData2} placeholder={"Password"} type={"password"}/>
+        
+       </div><br/>
+
+
+        <Button onClick={onClick} backgroundcolor="#44627C">SIGN IN</Button>
+     
+        
+    </div>
+</div>
+    
+    
+    </>
+}
+
+export default SignIn
---
+++ van-gogh-online-webstore/src/pages/SignUp/index.jsx
@@ -0,0 +1,58 @@
+import React,{ useState } from 'react'
+import './style.css'
+import Navbar from '../../components/Navbar'
+import Input from '../../components/TextInput'
+import Button from '../../components/Button'
+
+
+
+const SignUp = () => {
+
+    const [inputData1,setInputData1]= useState('')
+    const [inputData2,setInputData2]= useState('')
+    const [inputData3,setInputData3]= useState('')
+    const [inputData4,setInputData4]= useState('')
+    const [inputData5,setInputData5]= useState('')
+    const [inputData6,setInputData6]= useState('')
+
+    const onClick = () => {
+
+        console.log(inputData1);
+        console.log(inputData2);
+        console.log(inputData3);
+        console.log(inputData4);
+        console.log(inputData5);
+        console.log(inputData6);
+
+    }
+
+    return <> 
+   <Navbar /><br/><br/><br/>
+
+<div>
+    <div className='meio'>
+       <div className='signup'>
+       <span >Sign Up</span> 
+       </div>
+       <div>  
+
+        <Input  setInputData={setInputData1} placeholder={"Name"} type={"text"}/>
+        <Input  setInputData={setInputData2} placeholder={"Email"} type={"email"}/>
+        <Input  setInputData={setInputData3} placeholder={"Adress"} type={"text"}/>
+        <Input  setInputData={setInputData4} placeholder={"Telephone"} type={"tel"}/>
+        <Input  setInputData={setInputData5} placeholder={"Password"} type={"password"}/>
+        <Input  setInputData={setInputData6} placeholder={"Confirm Password"} type={"password"}/>
+        
+       </div><br/>
+        
+            <Button onClick={onClick} backgroundcolor="#A1C1AA">SIGN UP</Button>
+       
+        
+    </div>
+</div>
+    
+    
+    </>
+}
+
+export default SignUp
