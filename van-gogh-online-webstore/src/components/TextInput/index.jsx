7836660b4651872722809c13a681e505d75bc19c
tiagocbr <111984576+tiagocbr@users.noreply.github.com>
2023-05-22 04:44:04 -0300

    Add files via upload

---
+++ van-gogh-online-webstore/src/components/TextInput/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+const Input = ({placeholder,type,setInputData}) => {
+
+
+    const handleInputChange = (e) => {
+        setInputData(e.target.value)
+    }
+
+    return <>
+
+     <input onChange={handleInputChange} type={type} className="input" placeholder={placeholder}></input>
+    </>
+
+}
+
+export default Input 
