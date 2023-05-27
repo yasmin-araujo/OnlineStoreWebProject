2b2c5703109e6a4e637d06d0eb93c2f2ab32bbdd
Flavio Salles <flaviosalles3@usp.br>
2023-05-27 11:01:48 -0300

    modularizando perfil

--- van-gogh-online-webstore/src/pages/ProfilePage/index.js
+++ van-gogh-online-webstore/src/pages/ProfilePage/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import imagem from './profile-icons/vicent-self-portrait.jpg';
 import './style.css';
 import Navbar from '../../components/Navbar';
 import { TextField } from '@mui/material';
+import ProfileMenu from '../../components/ProfileMenu';
 
 export default function ProfilePage() {
 	let name = 'Van Gogh'
@@ -11,15 +11,7 @@ export default function ProfilePage() {
 	return(
 		<>
 			<Navbar/><br/><br/><br/><br/>
-			<div className='profileDiv'>
-				<img id='profileIcon' src={imagem}/>
-				<h2 id='profileName'>{name}</h2>
-			</div>
-			<div className='profileOptions'>
-				<button type="button" clas>Informations</button><br/>
-				<button type="button">Orders</button><br/> 
-				<button type="button">Logout</button><br/>
-			</div>
+			<ProfileMenu/>
 			<div className='informations'>
 				<h1>Informations</h1>
 				<TextField className="TextField" label="Nome" variant="outlined" margin='normal' defaultValue={name}/><br/>
