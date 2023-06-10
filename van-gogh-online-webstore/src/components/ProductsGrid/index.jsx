d46c300114e13ac83af0bcfbe34f3e4604f919a1
Yasmin Araujo <52540870+yasmin-araujo@users.noreply.github.com>
2023-06-10 14:53:08 -0300

    Retrieve filter values

--- van-gogh-online-webstore/src/components/ProductsGrid/index.jsx
+++ van-gogh-online-webstore/src/components/ProductsGrid/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 import { products } from "../../pages/ProductsPage/products";
 import Product from "../Product";
 
-export default function ProductsGrid() {
+export default function ProductsGrid({ filter }) {
     return (
         <div className="products-grid">
             {products.map((product, index) => <Product product={product} key={'product-' + index} />)}
