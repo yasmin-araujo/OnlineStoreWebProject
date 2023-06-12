0a678074a186ef7ed61ae61ce180ec7b3bd4c4b5
tiagocbr <tiagocbr06@hotmail.com>
2023-06-12 17:04:41 -0300

    Diminuir estoque quando add no carrinho
    aumentar estoque quando tira do carrinho

--- van-gogh-online-webstore/src/components/CartProduct/index.jsx
+++ van-gogh-online-webstore/src/components/CartProduct/index.jsx
@@ -2,35 +2,37 @@ import React, { useEffect, useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import './style.css';
 import NumberTextField from '../NumberTextField';
+import { products } from '../../utils/products';
 
 
 export default function CartProduct({ product, handleProductDeletion, handleProductAmount }) {
 	const [productAmount, setProductAmount] = useState(product.quantity)
 
-    const handleDeletionEvent = () => {
-        handleProductDeletion(product.id);
-    }
+	const handleDeletionEvent = () => {
+		handleProductDeletion(product.id);
+		products.find(element => element.id === product.id).qtd += product.quantity;
+	}
 
-    useEffect(() => {
-        if (productAmount === '') {
-            handleProductAmount(product.id, 1);
-            setProductAmount('1');
-        }
-        else {
-            handleProductAmount(product.id, parseInt(productAmount));
-        }
-    }, [productAmount]);
+	useEffect(() => {
+		if (productAmount === '') {
+			handleProductAmount(product.id, 1);
+			setProductAmount('1');
+		}
+		else {
+			handleProductAmount(product.id, parseInt(productAmount));
+		}
+	}, [productAmount]);
 
 	return (
 		<div className='cart-product'>
 			<div className='cart-product-info'>
-			<img id='cart-product-image' src={require('../../pages/ProductsPage/products-images/' + product.img)} alt={product.name + ' picture'} />
+				<img id='cart-product-image' src={require('../../pages/ProductsPage/products-images/' + product.img)} alt={product.name + ' picture'} />
 				<div>
 					<p><strong>{product.name}</strong></p>
 					<p>$ {product.price}</p>
 				</div>
 			</div>
-			<NumberTextField label={'Qty'} value={product.quantity} setValue={(value) => { setProductAmount(value) }} style={{ width: '80px', minWidth: '55px' }} min={1} max={30}/>
+			<NumberTextField label={'Qty'} value={product.quantity} setValue={(value) => { setProductAmount(value) }} style={{ width: '80px', minWidth: '55px' }} min={1} max={30} />
 			<button className='cart-delete-button' onClick={handleDeletionEvent}><DeleteIcon /></button>
 		</div>
 	);
