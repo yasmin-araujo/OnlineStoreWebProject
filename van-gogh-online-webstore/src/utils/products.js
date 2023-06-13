8dc37618e9b3c84326004e7bd30ee08ba351bf33
Yasmin Araujo <52540870+yasmin-araujo@users.noreply.github.com>
2023-06-13 17:13:17 -0300

    Add products

--- van-gogh-online-webstore/src/utils/products.js
+++ van-gogh-online-webstore/src/utils/products.js
@@ -1,36 +1,94 @@
+import { collectionsEnum } from "./collectionsEnum";
+
 export const products = [{
     id: 1,
-    name: 'Product',
-    price: 8.00,
+    name: 'Mug Vincent’s flowers',
+    price: 9.00,
     qtd: 50,
-    collection: 0,
+    collection: collectionsEnum.NONE.id,
     img: 'mug-vincents-flowers.jpg'
 }, {
     id: 2,
-    name: 'Unavailable Product',
-    price: 19.00,
-    qtd: 0,
-    collection: 0,
-    img: 'mug-vincents-flowers.jpg'
+    name: 'Cushion cover Sunflowers',
+    price: 11.00,
+    qtd: 1,
+    collection: collectionsEnum.SUNFLOWERS.id,
+    img: 'cushion-cover-sunflowers.jpg'
 }, {
     id: 3,
-    name: 'Product',
-    price: 88.00,
-    qtd: 50,
-    collection: 0,
-    img: 'mug-vincents-flowers.jpg'
+    name: 'Vincent doll',
+    price: 28.00,
+    qtd: 40,
+    collection: collectionsEnum.SELF_PORTRAITS.id,
+    img: 'vincent-doll.jpg'
 }, {
     id: 4,
-    name: 'Product Sunflowers Collection',
-    price: 18.00,
-    qtd: 50,
-    collection: 2,
-    img: 'mug-vincents-flowers.jpg'
+    name: 'Cushion cover Almond Blossom',
+    price: 11.00,
+    qtd: 24,
+    collection: collectionsEnum.ALMOND_BLOSSOM.id,
+    img: 'cushion-cover-almond-blossom.jpg'
 }, {
     id: 5,
-    name: 'Product',
-    price: 56.00,
+    name: 'Mouse pad Sunflowers',
+    price: 5.00,
     qtd: 50,
-    collection: 0,
-    img: 'mug-vincents-flowers.jpg'
+    collection: collectionsEnum.SUNFLOWERS.id,
+    img: 'mouse-pad-sunflowers.jpg'
+}, {
+    id: 6,
+    name: 'Notebook Almond Blossom print',
+    price: 10.00,
+    qtd: 150,
+    collection: collectionsEnum.ALMOND_BLOSSOM.id,
+    img: 'notebook-almond-blossom.jpg'
+}, {
+    id: 7,
+    name: 'Scarf Sunflowers',
+    price: 43.00,
+    qtd: 150,
+    collection: collectionsEnum.SUNFLOWERS.id,
+    img: 'scarf-sunflowers.jpg'
+}, {
+    id: 8,
+    name: 'Eyeglass case Almond blossom',
+    price: 10.00,
+    qtd: 150,
+    collection: collectionsEnum.ALMOND_BLOSSOM.id,
+    img: 'eyeglass-almond-blossom.jpg'
+}, {
+    id: 9,
+    name: 'Socks Self-Portrait',
+    price: 11.00,
+    qtd: 98,
+    collection: collectionsEnum.SELF_PORTRAITS.id,
+    img: 'socks-self-portrait.jpg'
+}, {
+    id: 10,
+    name: 'Starry Night Umbrella',
+    price: 50.00,
+    qtd: 13,
+    collection: collectionsEnum.STARRY_NIGHT.id,
+    img: 'umbrella-starry-night.jpeg'
+}, {
+    id: 11,
+    name: 'LEGO® Ideas The Starry Night',
+    price: 185.00,
+    qtd: 9,
+    collection: collectionsEnum.STARRY_NIGHT.id,
+    img: 'lego-starry-night.jpg'
+}, {
+    id: 12,
+    name: 'Enamel Holiday Ornament Starry Night',
+    price: 16.00,
+    qtd: 67,
+    collection: collectionsEnum.STARRY_NIGHT.id,
+    img: 'enamel-ornament-starry-night.jpg'
+}, {
+    id: 13,
+    name: 'Starry Night Mug',
+    price: 19.00,
+    qtd: 267,
+    collection: collectionsEnum.STARRY_NIGHT.id,
+    img: 'mug-starry-night.jpg'
 }];
