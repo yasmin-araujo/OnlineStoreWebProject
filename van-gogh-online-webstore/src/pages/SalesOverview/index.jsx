cb43030b1ac6b24807389176bf59d441e5440f1f
Yasmin Araujo <52540870+yasmin-araujo@users.noreply.github.com>
2023-07-10 22:21:43 -0400

    Remove Sales Overview page

--- van-gogh-online-webstore/src/pages/SalesOverview/index.jsx
+++ van-gogh-online-webstore/src/pages/SalesOverview/index.jsx
@@ -4,26 +4,24 @@ import './style.css';
 
 import Navbar from '../../components/Navbar';
 import ProfileMenu from '../../components/ProfileMenu';
-import TabelaSales from '../../components/TabelaSales';
 import ProfileLogout from '../../components/ProfileLogout';
 import ManageUsers from '../../components/ManageUsers';
-
 
 export default function SalesOverview() {
     useEffect(() => {
         document.body.style.backgroundColor = '#FFF'
-    }, [])
-    const pages = [<TabelaSales />, <ManageUsers />, <ProfileLogout />]
-    const [profilePage, setProfilePage] = useState(pages[0])
+    }, []);
+    const pages = [<ManageUsers />, <ProfileLogout />];
+    const [profilePage, setProfilePage] = useState(pages[0]);
     const handleProfileChange = (index) => {
-        setProfilePage(pages[index])
+        setProfilePage(pages[index]);
     }
 
     return (
         <>
             <Navbar bgColor='white' />
             <div className='profile-so'>
-                <ProfileMenu profilePages={['Sales Overview', 'Manage users', 'Logout']} handleProfileChange={handleProfileChange} />
+                <ProfileMenu profilePages={['Manage users', 'Logout']} handleProfileChange={handleProfileChange} />
                 {profilePage}
             </div>
         </>
